8dcc1d7d4bc9c450265aa1af50f5e24817763757
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-06-13 10:51:33 +0530

    event model updated

--- src/resources/event/event.model.js
+++ src/resources/event/event.model.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const eventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true, trim: true },
-    date: { type: Date },
-    createdBy: { type: mongoose.Schema.Types.ObjectId }
+    date: { type: String },
+    created_by: { type: mongoose.Schema.Types.ObjectId }
 });
 
 module.exports = mongoose.model('Event', eventSchema);
