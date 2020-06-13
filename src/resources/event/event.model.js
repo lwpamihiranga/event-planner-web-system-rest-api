dc4ef79b12ddb6d38f58d10fa1ff9c70cd1b0045
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-06-13 16:17:07 +0530

    event model update

--- src/resources/event/event.model.js
+++ src/resources/event/event.model.js
@@ -4,6 +4,7 @@ const eventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true, trim: true },
     date: { type: String },
+    isComplete: { type: Boolean, default: false },
     created_by: { type: mongoose.Schema.Types.ObjectId }
 });
 
