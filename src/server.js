54aee18048ed77dd4aaf47019c9314633216d625
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-06-13 10:52:07 +0530

    event route added

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const cors          = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
+const databaseConnector = require('./utils/db');
+
+const eventRouter = require('./resources/event/event.router');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -12,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/api/event', eventRouter);
+
 app.listen(PORT, () => {
+    databaseConnector.connectDatabase();
     console.log(`REST API on http://localhost:${PORT}/api`);
 });
